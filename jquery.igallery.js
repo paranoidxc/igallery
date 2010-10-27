c6e584c56e6efe970ffc89a4f8bab0d114c5899f
Paranoid.xc <paranoid.xc@gmail.com>
2010-10-27 16:16:44 +0800

    organazation js

--- jquery.igallery.js
+++ jquery.igallery.js
@@ -7,7 +7,7 @@
     var id,img_path;
     var iterator = -1;
     var igallery_identfiy_code , transition_identify;
-    var tf,tf_imgs;
+    var tf,tf_imgs,tf_width;
     var photo1,photo2,undock,overlay,next,prev,overlay_tips;
 
 
@@ -82,48 +82,109 @@
     }
 
 
-    /************************************************/
-    /* Main plugin code         */
-    /* set gallery prototype */
-    /************************************************/
- 	  return this.each(function() {
-      igallery = $(this);
-      tf = igallery.find('.thumb_frame');
-      tf_imgs = tf.find('img');
 
-      id = igallery.attr('id');
-
-      igallery_identfiy_code = id+''+ (Math.random()+'').replace('.','');
-      transition_identify = 'transition' + igallery_identfiy_code;
-
+    function css_igallery(){
       igallery.css({
         'width'     : opts.gallery_width,
         'height'    : opts.gallery_height,
         'border'    : opts.gallery_border,
         'background': opts.gallery_bg,
         'position'  : opts.gallery_position
+//        'margin'    : opts.gallery_margin
       });
+    };
 
-        /*'margin'    : opts.gallery_margin */
-      var tf_width = tf_imgs.length*opts.thumb_width +tf_imgs.length*10;
+    function build_overlay() {
+      overlay = $('<div/>')
+        .attr('id', 'overlay'+igallery_identfiy_code)
+        .css({
+            'opacity': '0.5', 'background':'#000',
+            'position': 'absolute', 'bottom': '0',
+            'height':  tf.height()+14,
+            'z-index': '1002',
+            'width': '100%' })
+        .prependTo(igallery);
+    };
+
+    function build_controller() {
+      next = $('<img />')
+        .attr('id','next'+igallery_identfiy_code)
+        .attr('src', img_path+'next.png')
+        .css({
+            'position':'absolute',
+            'cursor': 'pointer',
+            'z-index':  '100000',
+            'vertical-align':'middle',
+            'left':tf_width + tf.position().left
+            })
+      .prependTo(igallery);
+
+      prev = $('<img />')
+        .attr('id','prev'+igallery_identfiy_code)
+        .attr('src', img_path+'prev.png')
+        .css({
+            'position':'absolute',
+            'cursor': 'pointer',
+            'z-index':  '100000',
+            'vertical-align': 'bottom',
+            'left':tf.position().left-30
+            })
+      .prependTo(igallery);
+
+      next.css({ 'bottom': (overlay.height()-22)/2});
+      prev.css({ 'bottom': (overlay.height()-22)/2 });
+      next.click(showNextItem);
+      prev.click(showPrevItem);
+
+      undock = $('<img />')
+        .attr('src', img_path+'undock.png')
+        .addClass('undock'+igallery_identfiy_code)
+        .css({ 'z-index': 100000, 'position': 'absolute', 'cursor': 'pointer', 'bottom': '15px','right': '5px' })
+        .attr('title',"Undock into separate")
+        .prependTo(igallery);
+      undock.css({ 'bottom': (overlay.height()-22)/2 });
+      undock.click(undockCL);
+
+    };
+
+    function build_dis_photo() {
+      photo1 = $('<img />')
+        .attr({ 'src' : $(tf_imgs[0]).attr('source_src'), 'id': 'photo1'+igallery_identfiy_code})
+        .addClass(igallery_identfiy_code)
+        .prependTo(igallery);
+      
+      photo2 = $('<img />')
+        .attr({ 'src' : $(tf_imgs[0]).attr('source_src'), 'id': 'photo2'+igallery_identfiy_code})
+        .addClass(igallery_identfiy_code)
+        .prependTo(igallery);
+
+      $(photo1).css({ 'position': 'absolute', 'top': '0','left':'0', 'width':opts.gallery_width, 'height':opts.gallery_height });
+      $(photo2).css({ 'position': 'absolute', 'top': '0','left':'0', 'width':opts.gallery_width, 'height':opts.gallery_height });
+    }
+    /************************************************/
+    /* Main plugin code         */
+    /* set gallery prototype */
+    /************************************************/
+ 	  return this.each(function() {
+
+      igallery = $(this);
+      tf = igallery.find('.thumb_frame');
+      tf_imgs = tf.find('img');
+      id = igallery.attr('id');
+      igallery_identfiy_code = id+''+ (Math.random()+'').replace('.','');
+      transition_identify = 'transition' + igallery_identfiy_code;
+      
+      css_igallery();
+      tf_width = tf_imgs.length*opts.thumb_width +tf_imgs.length*10;
       tf.css({'width': tf_width, 
         'z-index': '1003',
         'position': 'absolute',
         'left'  : (opts.gallery_width-tf_width)/2
       });
 
-      overlay = $('<div/>')
-                  .attr('id', 'overlay'+igallery_identfiy_code)
-                  .css({
-                      'opacity': '0.5', 'background':'#000',
-                      'position': 'absolute', 'bottom': '0',
-                      'height':  tf.height()+14,
-                      'z-index': '1002',
-                      'width': '100%' })
-                  .prependTo(igallery);
-      
-      tf.css({'bottom': (overlay.height() - tf.height())/2});
+      build_overlay();
 
+      tf.css({'bottom': (overlay.height() - tf.height())/2});
 
       //Determine path between current page and filmstrip images
 			//Scan script tags and look for path to IGallery plugin
@@ -134,54 +195,8 @@
 				}
 			});
 
-      next = $('<img />')
-              .attr('id','next'+igallery_identfiy_code)
-              .attr('src', img_path+'next.png')
-              .css({
-                  'position':'absolute',
-                  'cursor': 'pointer',
-                  'z-index':  '100000',
-                  'vertical-align':'middle',
-                  'left':tf_width + tf.position().left
-              })
-              .prependTo(igallery);
-      next.click(showNextItem);
-
-      prev = $('<img />')
-              .attr('id','prev'+igallery_identfiy_code)
-              .attr('src', img_path+'prev.png')
-              .css({
-                  'position':'absolute',
-                  'cursor': 'pointer',
-                  'z-index':  '100000',
-                  'vertical-align': 'bottom',
-                  'left':tf.position().left-30
-              })
-              .prependTo(igallery);
-
-      next.css({ 'bottom': (overlay.height()-22)/2});
-      prev.css({ 'bottom': (overlay.height()-22)/2 });
-      prev.click(showPrevItem);
-
-      photo1 = $('<img />')
-        .attr({ 'src' : $(tf_imgs[0]).attr('source_src'), 'id': 'photo1'+igallery_identfiy_code})
-        .addClass(igallery_identfiy_code)
-        .prependTo(igallery);
-
-      photo2 = $('<img />')
-        .attr({ 'src' : $(tf_imgs[0]).attr('source_src'), 'id': 'photo2'+igallery_identfiy_code})
-        .addClass(igallery_identfiy_code)
-        .prependTo(igallery);
-
-      $(photo1).css({ 'position': 'absolute', 'top': '0','left':'0', 'width':opts.gallery_width, 'height':opts.gallery_height });
-      $(photo2).css({ 'position': 'absolute', 'top': '0','left':'0', 'width':opts.gallery_width, 'height':opts.gallery_height });
-
-      undock = $('<img />')
-                .attr('src', img_path+'undock.png')
-                .addClass('undock'+igallery_identfiy_code)
-                .css({ 'z-index': 100000, 'position': 'absolute', 'cursor': 'pointer', 'bottom': '15px','right': '5px' })
-                .attr('title',"Undock into separate")
-                .prependTo(igallery);
+      build_controller();
+      build_dis_photo();
 
       if( opts.overlay_tips ) {
         overlay_tips = $('<div/>')
@@ -195,11 +210,6 @@
                           })
                       .prependTo(igallery);
       }
-      
-      undock.css({ 'bottom': (overlay.height()-22)/2 });
-
-      undock.click(undockCL);
-      showNextItem()
 
       tf_imgs.click(function(){
         $(document).stopTime(transition_identify);
@@ -216,6 +226,7 @@
         });
       });
 
+      showNextItem()
     });/* END Main plugin code */
 
   };
@@ -235,6 +246,7 @@
     'gallery_position':'relative',
     'theme'           :'light',
     'overlay_tips'    : false,
+    'overlay'         : false,
     'thumb_width'     :50,
     'thumb_height'    :50,
     'speed'           :400,
